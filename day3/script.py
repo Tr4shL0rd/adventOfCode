cccfd995f9ef68cf5471eb0f256cdce1038f4762
Tr4shL0rd <ketil04@gmail.com>
2022-12-14 13:03:00 +0100

    day3

--- day3/script.py
+++ day3/script.py
@@ -7,9 +7,10 @@ def attempt1():
     for letter in ALPHABET:
         letter_priority[letter] = score
         score += 1
-    print(letter_priority)
-    exit()
-    
+    #print(letter_priority)
+    #exit()
+    total_sum = 0
+    priorities = []
     with open("input.txt", "r") as f:
         lines = f.readlines()
         lineNum = 1
@@ -19,6 +20,15 @@ def attempt1():
             print(f"line {lineNum}\n\tcompartment1: {comp1}\n\tcompartment2: {comp2}")
             for let1 in comp1:
                 if let1 in comp2:
-                    print(f"{let1} in comp1 & comp2")
+                    print(f"\"{let1}\" in comp1 & comp2")
+                    total_sum += letter_priority[let1]
+                    priorities.append(let1)
+                    #total_sum.append(let1)
+            #print(total_sum)
+            print(priorities)
+            for p in priorities:
+                print(letter_priority[p])
+            #for let in total_sum:
+                #total_sum.append(letter_priority[let])
             #input()
 attempt1()
